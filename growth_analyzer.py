82d2fee73092f095a07a97079ea95d9e4a1c17cc
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-03-11 11:48:03 +0900

    1.0.7
    - removed use_gaussian parameter

--- growth_analyzer.py
+++ growth_analyzer.py
@@ -1,8 +1,8 @@
 # Growth Analyzer
 
 # Author: Tomio Kobayashi
-# Version 1.0.7
-# Updated: 2024/03/10
+# Version 1.0.8
+# Updated: 2024/03/11
 
 import requests
 import time
@@ -16,19 +16,19 @@ from scipy.optimize import curve_fit
     
 class growth_analyzer:
 
-    def analyzer(data, xcols, use_lasso=False, use_gaussian=False):
+    def analyzer(data, xcols, use_lasso=False):
     #     col 0: Timeline
     #     col 1-: Variables
         if use_lasso:
             data = [d[1:] + [d[0]] for d in data]
             print("data", data)
-            res = relation_finder.find_relations(data, "", "Timeline", cols=xcols, const_thresh=0.1, skip_inverse=False, use_lasso=use_lasso, use_gaussian=use_gaussian)
+            res = relation_finder.find_relations(data, "", "Timeline", cols=xcols, const_thresh=0.1, skip_inverse=False, use_lasso=use_lasso)
             return res
         else:
             res = []
             for i, c in enumerate(xcols):
                 pdata = [[row[0], row[i+1]] for row in data]
-                ret = relation_finder.find_relations(pdata, c, "Timeline", const_thresh=0.1, skip_inverse=True, use_lasso=use_lasso, use_gaussian=use_gaussian)
+                ret = relation_finder.find_relations(pdata, c, "Timeline", const_thresh=0.1, skip_inverse=True, use_lasso=use_lasso)
                 res.append(ret)
             return res
     
