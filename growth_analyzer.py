06d0cfa5bd3a21989912b16bcb5f40cbad02fa56
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-03-12 20:13:32 +0900

    1.1.0

--- growth_analyzer.py
+++ growth_analyzer.py
@@ -1,8 +1,8 @@
 # Growth Analyzer
 
 # Author: Tomio Kobayashi
-# Version 1.0.8
-# Updated: 2024/03/11
+# Version 1.1.0
+# Updated: 2024/03/12
 
 import requests
 import time
@@ -16,19 +16,23 @@ from scipy.optimize import curve_fit
     
 class growth_analyzer:
 
-    def analyzer(data, xcols, use_lasso=False):
+    def __init__(self):
+        self.finder = relation_finder()
+        
+    def analyzer(self, data, xcols, use_lasso=False, skip_outliers=False):
     #     col 0: Timeline
     #     col 1-: Variables
         if use_lasso:
             data = [d[1:] + [d[0]] for d in data]
-            print("data", data)
-            res = relation_finder.find_relations(data, "", "Timeline", cols=xcols, const_thresh=0.1, skip_inverse=False, use_lasso=use_lasso)
+            res = self.finder.find_relations2(data, "", "Timeline", cols=xcols, const_thresh=0.1, skip_inverse=False, skip_outliers=skip_outliers, use_lasso=use_lasso, xy_switch=True)
             return res
         else:
             res = []
             for i, c in enumerate(xcols):
                 pdata = [[row[0], row[i+1]] for row in data]
-                ret = relation_finder.find_relations(pdata, c, "Timeline", const_thresh=0.1, skip_inverse=True, use_lasso=use_lasso)
+                ret = self.finder.find_relations2(pdata, c, "Timeline", const_thresh=0.1, skip_inverse=True, skip_outliers=skip_outliers, use_lasso=use_lasso, xy_switch=True)
                 res.append(ret)
             return res
     
+    def predict(self, x):
+        return self.finder.predict(x)
